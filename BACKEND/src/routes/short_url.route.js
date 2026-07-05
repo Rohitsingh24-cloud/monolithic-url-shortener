e4278ebe310ad7bd011130cb2e83f366ea7f2a96
rohit <rohitsin5312@gmail.com>
2026-07-05 06:54:38 +0530

    Initial commit

---
+++ BACKEND/src/routes/short_url.route.js
@@ -0,0 +1,6 @@
+import express from "express";
+const router=express.Router();
+import { createShortUrl } from "../controller/short_url.controller.js";
+router.post("/",createShortUrl);
+
+export default router;
---
+++ FRONTEND/src/api/shortUrl.api.js
@@ -0,0 +1,5 @@
+import axiosInstance from "../utils/axiosInstance.js"
+export const createShortUrl = async (url,slug) =>{
+    const {data} = await axiosInstance.post("/api/create",{url,slug})
+    return data.shortUrl
+}
